3435f5dc2681b5c1c91c10af82ac324107809663
Tianyu Fang <t@tfang.info>
2013-08-30 12:54:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<?php while($this->next()): ?>
         <div class="post">
 			<h2 class="entry_title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-        <p class="entry_data">Posted on <?php $this->date('Y/n/j'); ?> / Categorized: <?php $this->category(','); ?> / <?php Views_Plugin::theViews('', ' Comments'); ?> / <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('No Comments', '1 Comment', '%d Comments'); ?></a></p>
+        <p class="entry_data">Posted on <?php $this->date('Y/n/j'); ?> / Categorized: <?php $this->category(','); ?> / <?php Views_Plugin::theViews('', ' Views'); ?> / <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('No Comments', '1 Comment', '%d Comments'); ?></a></p>
 			<?php $this->content('Show all...'); ?>
         </div>
 	<?php endwhile; ?>
