be58912e8ffeb00e1d8ed38009238093aa426e98
Tianyu Fang <t@tfang.info>
2013-08-30 12:42:56 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+</div>	<div id="footer">
+	<a href="<?php $this->options->siteurl(); ?>"><?php $this->options->title(); ?></a> <?php _e('is powered by'); ?> <a href="http://www.typecho.org">Typecho)))</a> & <a href="http://www.microhu.com">Default-M Theme</a>
+  Typecho))) en-US translated	by <a href="http://赵阳.cn/">Young</a>, <a href="http://tfang.info">Tianyu</a> with support of the all the Typecho fans...
+  <br><a href="http://https://github.com/typecho-en/">Typecho-en Project</a> / <a href="http://tefans.org/>Tefans.org</a>
+	</div><!-- end #footer -->
+<?php $this->footer(); ?>
+</body>
+</html>
